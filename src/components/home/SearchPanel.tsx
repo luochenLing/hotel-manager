03f5e2c8416dd35493ef69142cf6e58aaef8bad6
@yulianwei <yulianwei@hztianque.com>
2020-10-28 09:04:33 +0800

    v1.0.0.0

--- src/components/home/SearchPanel.tsx
+++ src/components/home/SearchPanel.tsx
@@ -1,51 +1,52 @@
-import React from "react";
-import "css/home/search-panel.scss";
-import { Tabs, Tab, Toolbar, Link } from "framework7-react";
-import Calendar from "components/common/Calendar";
-import KeyWord from "components/home/keyWord/Index";
-import { getWeek,getDayByNum } from "utils/common";
-import {connect} from 'react-redux'
-import {setShowKeyWord} from 'redux/action'
+import React from 'react'
+import 'css/home/search-panel.scss'
+import { Tabs, Tab, Toolbar, Link } from 'framework7-react'
+import Calendar from 'components/common/Calendar'
+import KeyWord from 'components/home/keyWord/Index'
+import { getWeek, getDayByNum } from 'utils/common'
+import { connect } from 'react-redux'
+import { setShowKeyWord } from 'redux/action'
 
-const SearchForm = React.lazy(() => import("components/home/SearcForm"));
+const SearchForm = React.lazy(() => import('components/home/SearcForm'))
 
-let dayDiff = 1; //日差
 type stateType = {
-  showCalendar: boolean;
-  calendarInline: any;
-  startDay: string;
-  endDay: string;
-  yesterday: string;
-  fromWeek: string;
-  toWeek: string;
-};
+  showCalendar: boolean
+  calendarInline: any
+  startDay: string
+  endDay: string
+  dayDiff: number
+  yesterday: string
+  fromWeek: string
+  toWeek: string
+}
 type propsType = {
-  setShowKeyWord:Function;
-  keyWord:boolean;
-};
-const calendarRef = React.createRef<any>();
+  setShowKeyWord: Function
+  keyWord: boolean
+}
+const calendarRef = React.createRef<any>()
 class SearchPanel extends React.Component<propsType, stateType> {
   constructor(props: any) {
-    super(props);
+    super(props)
     this.state = {
       showCalendar: false,
-      calendarInline: "",
-      startDay:"",
-      endDay:"",
-      yesterday: "",
-      fromWeek: "",
-      toWeek: "",
-    };
+      calendarInline: '',
+      startDay: '',
+      endDay: '',
+      dayDiff: 0,
+      yesterday: '',
+      fromWeek: '',
+      toWeek: '',
+    }
   }
   componentDidMount() {
-    this.initCalendarDate();
-    this.initKeyWordPanel();
+    this.initCalendarDate()
+    this.initKeyWordPanel()
   }
 
   /**
    * 初始化关键字面板
    */
-  initKeyWordPanel(){
+  initKeyWordPanel() {
     this.props.setShowKeyWord(false)
   }
 
@@ -54,73 +55,77 @@ class SearchPanel extends React.Component<propsType, stateType> {
    */
   initCalendarDate = () => {
     let calendarInfo = {
-      startDay:  new Date().Format('yyyy-MM-dd'),
-      endDay: getDayByNum(new Date(),7).Format('yyyy-MM-dd'),
-      fromWeek: "",
-      toWeek:"",
-    };
-    calendarInfo.fromWeek = getWeek(calendarInfo.startDay);
-    calendarInfo.fromWeek = getWeek(calendarInfo.toWeek);
-    this.setState(calendarInfo,()=>{
+      startDay: new Date().Format('yyyy-MM-dd'),
+      endDay: getDayByNum(new Date(), 7).Format('yyyy-MM-dd'),
+      dayDiff: 1,
+      fromWeek: '',
+      toWeek: '',
+    }
+    calendarInfo.fromWeek = getWeek(calendarInfo.startDay)
+    calendarInfo.toWeek = getWeek(calendarInfo.endDay)
+    calendarInfo.dayDiff = this.getDayDiff(calendarInfo)
+    this.setState(calendarInfo, () => {
       //setstate的异步原因，所以要初始化state以后再去根据新的state做操作
-      this.getYesterday();
-    });
-  };
+      this.getYesterday()
+    })
+  }
 
   /**
    * 获取昨天
    */
   getYesterday = () => {
-    let day = new Date(this.state.startDay);
-    let yesterday = getDayByNum(day,-1)
-    this.setState({ yesterday: yesterday.Format("yyyy-M-d") });
-  };
+    let day = new Date(this.state.startDay)
+    let yesterday = getDayByNum(day, -1)
+    this.setState({ yesterday: yesterday.Format('yyyy-M-d') })
+  }
 
   /**
    * 打开日历
    * @param showCalendar
    */
   getCalendar = (showCalendar: boolean) => {
-    this.setState({ showCalendar });
-  };
-  
+    this.setState({ showCalendar })
+  }
+
   /**
    * 打开关键词面板
-   * @param showKeyWork 
+   * @param showKeyWork
    */
-  openKeyWord= () => {
+  openKeyWord = () => {
     this.props.setShowKeyWord(true)
-  };
-  
+  }
+
   /**
-   * 设置范围
+   * 获取日期间隔
    * @param dateArr
    */
-  setDateRange = (dateArr: {
-    startDay: string;
-    endDay: string;
-    fromWeek: string;
-    toWeek: string;
+  getDayDiff = (dateArr: {
+    startDay: string
+    endDay: string
+    fromWeek?: string
+    toWeek?: string
   }) => {
-    let sDay = new Date(dateArr.startDay);
-    let eDay = new Date(dateArr.endDay);
-    dayDiff = Math.abs(sDay.getTime() - eDay.getTime()) / 1000 / 60 / 60 / 24;
-  };
+    let sDay = new Date(dateArr.startDay)
+    let eDay = new Date(dateArr.endDay)
+    return Math.ceil(Math.abs(sDay.getTime() - eDay.getTime()) / 1000 / 60 / 60 / 24); 
+  }
 
   /**
    * 关闭日历，关闭时获取选中值
    */
   closeCalendar = () => {
     this.setState({ showCalendar: false }, () => {
-      let dateArr = calendarRef.current.getSelDateArr();
+      let dateArr = calendarRef.current.getSelDateArr()
+      let dayDiff = this.getDayDiff({startDay:dateArr.startDay,endDay:dateArr.endDay});
       this.setState({
         startDay: dateArr.startDay,
         endDay: dateArr.endDay,
         fromWeek: dateArr.fromWeek,
         toWeek: dateArr.toWeek,
-      });
-    });
-  };
+        dayDiff,
+      })
+    })
+  }
 
   render() {
     const {
@@ -130,27 +135,26 @@ class SearchPanel extends React.Component<propsType, stateType> {
       showCalendar,
       fromWeek,
       toWeek,
-    } = this.state;
+    } = this.state
     return (
       <>
-        <div className="search-panel">
+        <div className='search-panel'>
           <Toolbar tabbar bottom>
-            <Link tabLink="#tab-1" tabLinkActive>
+            <Link tabLink='#tab-1' tabLinkActive>
               国内
             </Link>
-            <Link tabLink="#tab-2">国外</Link>
-            <Link tabLink="#tab-3">钟点房</Link>
+            <Link tabLink='#tab-2'>国外</Link>
+            <Link tabLink='#tab-3'>钟点房</Link>
           </Toolbar>
           <Tabs animated>
-            <Tab id="tab-1" className="page-content" tabActive>
+            <Tab id='tab-1' className='page-content' tabActive>
               <div
                 style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  backgroundColor: "#fff",
-                }}
-              >
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  backgroundColor: '#fff',
+                }}>
                 <SearchForm
                   startDay={startDay}
                   endDay={endDay}
@@ -158,19 +162,18 @@ class SearchPanel extends React.Component<propsType, stateType> {
                   toWeek={toWeek}
                   getCalendar={this.getCalendar}
                   openKeyWord={this.openKeyWord}
-                  dayDiff={dayDiff}
+                  dayDiff={this.state.dayDiff}
                 />
               </div>
             </Tab>
-            <Tab id="tab-2" className="page-content">
+            <Tab id='tab-2' className='page-content'>
               <div
                 style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  backgroundColor: "#fff",
-                }}
-              >
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  backgroundColor: '#fff',
+                }}>
                 <SearchForm
                   startDay={startDay}
                   endDay={endDay}
@@ -178,19 +181,18 @@ class SearchPanel extends React.Component<propsType, stateType> {
                   toWeek={toWeek}
                   getCalendar={this.getCalendar}
                   openKeyWord={this.openKeyWord}
-                  dayDiff={dayDiff}
+                  dayDiff={this.state.dayDiff}
                 />
               </div>
             </Tab>
-            <Tab id="tab-3" className="page-content">
+            <Tab id='tab-3' className='page-content'>
               <div
                 style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  backgroundColor: "#fff",
-                }}
-              >
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  backgroundColor: '#fff',
+                }}>
                 <SearchForm
                   startDay={startDay}
                   endDay={endDay}
@@ -198,7 +200,7 @@ class SearchPanel extends React.Component<propsType, stateType> {
                   toWeek={toWeek}
                   getCalendar={this.getCalendar}
                   openKeyWord={this.openKeyWord}
-                  dayDiff={dayDiff}
+                  dayDiff={this.state.dayDiff}
                 />
               </div>
             </Tab>
@@ -215,11 +217,13 @@ class SearchPanel extends React.Component<propsType, stateType> {
         />
         {/* 这里不再用state去控制开关，在redux中的话，使用props的属性去读取state状态，
         这是因为使用了reactredux所以让状态的读取通过外面的（app.js）Provider的store就可以达到读取属性的效果 */}
-        <KeyWord showKeyWord={this.props.keyWord}/>
+        <KeyWord showKeyWord={this.props.keyWord} />
       </>
-    );
+    )
   }
 }
 //state这里放的是reducer里面指定方法的state，比如reducer的keyWorkOption方法的state这里就可以取到
 //第二个参数是需要用到的action的方法，这些方法在这里注册以后可以用props的方式调用
-export default connect((state:any)=>({keyWord:state.keyWorkOption}),{setShowKeyWord})(SearchPanel);
+export default connect((state: any) => ({ keyWord: state.keyWorkOption }), {
+  setShowKeyWord,
+})(SearchPanel)
