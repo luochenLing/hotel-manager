1383f5646b3cbd7eb9de0225efa54a66c08ffd64
@yulianwei <yulianwei@hztianque.com>
2021-01-15 14:04:50 +0800

    v1.0.0.0

--- src/views/hotel/Info.tsx
+++ src/views/hotel/Info.tsx
@@ -1,19 +1,31 @@
-import React from "react";
-import "css/hotel/hotel-info.scss";
-import Banner from "components/hotel/Banner";
-type stateTypes = {};
-type propsTypes = {};
-class Info extends React.Component<propsTypes, stateTypes> {
+import React from 'react'
+import 'css/common/nav.scss'
+import Banner from 'components/common/Banner'
+import { RouteComponentProps } from 'react-router-dom'
+type stateTypes = {
+  infoList:{ url: string; alt: string }[]
+}
+type propsTypes = {}
+class Info extends React.Component<RouteComponentProps&propsTypes, stateTypes> {
   constructor(props: any) {
-    super(props);
-    this.state = {};
+    super(props)
+    this.state = {
+      infoList: [
+        { url: 'assets/images/hotel/info-2.png', alt: '详情2' },
+        { url: 'assets/images/hotel/info-1.png', alt: '详情1' },
+      ],
+    }
+  }
+  backFunc=()=>{
+    this.props.history.goBack()
   }
   render() {
+    const {infoList} =this.state
     return (
       <>
-        <Banner></Banner>
+        <Banner infoList={infoList} backFunc={this.backFunc}></Banner>
       </>
-    );
+    )
   }
 }
-export default Info;
+export default Info
