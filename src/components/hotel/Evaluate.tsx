7cbb6aa3d419e7c26df32f837172aae0185943ae
@yulianwei <yulianwei@hztianque.com>
2021-06-07 14:01:10 +0800

    css module file add

--- src/components/hotel/Evaluate.tsx
+++ src/components/hotel/Evaluate.tsx
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
-// import styles from 'css/home/hotel/evaluate.module.scss'
+import React from "react";
+import styles from "css/hotel/evaluate.module.scss";
+import { Card } from "framework7-react";
 interface propsTypes {}
 
 function EvaluateDom(props: propsTypes) {
-  return <>{/* <div className={styles["title"]}>住客评价</div> */}</>;
+  return (
+    <>
+      <Card
+        className={styles["evaluate"]}
+        outline
+        title="住客评价"
+        content="Card with header and footer. Card headers are used to display card titles and footers for additional information or just for custom actions."
+        footer="Card footer"
+      ></Card>
+    </>
+  );
 }
 const IntroCard = React.forwardRef((props: any, ref: any) => {
   return <EvaluateDom {...props} myRef={ref}></EvaluateDom>;
